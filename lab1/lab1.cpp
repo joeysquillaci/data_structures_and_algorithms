e4d13fdd7b47f5cf3ff00d4b7d20aa888239260e
Joey Squillaci <jsquillaci@gmail.com>
2023-08-28 17:41:58 -0500

    Update description of lab1 code
    Update of comments for lab 1 code

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,3 +1,5 @@
+// Create a program that counts the number of words in an inputted string.
+
 #include <iostream>
 #include <string>
 
@@ -5,16 +7,17 @@ using namespace std;
 
 int main() {
 
+   // Define string and get from user
    string sentence;
-
    cout << "Please input a sentence: " << endl;
-
    getline(cin, sentence);
 
+   // Initialize variables for parsing through string
    int count = 0;
    int end_of_string = 0;
    int index = 0;
 
+   // Parses through string based on number of spaces
    while(true) {
       string word = sentence.substr(index, index + sentence.find(" "));
       index = word.length() + index;
@@ -27,6 +30,7 @@ int main() {
       }
    }
 
+   // Output number of words to user
    cout << "The amount of words in {" << sentence << "} is " << count << endl;
 
    return 0;
